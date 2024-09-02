ce43acd05b07356f4dd79fd316ce511e4e56ea80
GrupoProjetoAplicado <162191804+GrupoProjetoAplicado@users.noreply.github.com>
2024-09-02 14:57:13 -0300

    Add files via upload

---
+++ Data/Análise Exploratória/Notebook Jupyter (Dados de prosperidade).py
@@ -0,0 +1,13 @@
+
+# Bibliotecas usadas na análise:
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Dados do arquivo CSV
+dados = pd.read_csv('data.csv')
+
+# Métodos para explorar os dados
+# Exibe as primeiras linhas do arquivo
+print(dados.head())
+
